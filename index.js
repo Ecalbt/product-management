602aaeb449757547991d27598a348fd3820acedc
Ecalbt <nat10a3tlvp@gmail.com>
2024-09-23 11:13:32 +0700

    cloud data

--- index.js
+++ index.js
@@ -30,10 +30,11 @@ app.use(flash());
 // end flash
 
 
-app.set('views', './views')
+app.set('views', '${__dirname}/views')
 app.set('view engine', 'pug')
 
-app.use(express.static('public'))
+
+app.use(express.static(`${__dirname}/public`))
 
 //App local variables:
 app.locals.prefixAdmin = systemConfig.prefixAdmin;
